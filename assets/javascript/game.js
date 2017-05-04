a6261f3d472ccace9c56138ec7294c050e27dfc6
John Bendfeldt <jfbendfeldt@gmail.com>
2017-05-03 21:57:51 -0500

    changed site structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,36 +11,36 @@ $(document).ready(function() {
     // creating objects and assigning them to an array 
 
     var firstPlayer = {
-        name: "Obi-Wan Kenobi",
+        name: 'Obi-Wan Kenobi',
 		healthPoints: 150,
-		attackPower: 35,
+		attackPower: 15,
 		counterAttackPower: 30,
-        numberOfAttacks: .25
+        numberOfAttacks: 1
 
     }
 
     var secondPlayer = {
-        name: "Darth Maul",
+        name: 'Darth Maul',
 		healthPoints: 120,
-		attackPower: 30,
-		counterAttackPower: 25,
-        numberOfAttacks: .25
+		attackPower: 15,
+		counterAttackPower: 20,
+        numberOfAttacks: 1
     }
 
     var thirdPlayer = {
-        name: "Jar Jar Binks",
+        name: 'Jar Jar Binks',
 		healthPoints: 200,
-		attackPower: 55,
-		counterAttackPower: 50,
-        numberOfAttacks: .25
+		attackPower: 25,
+		counterAttackPower: 30,
+        numberOfAttacks: 1
     }
 
     var fourthPlayer = {
-        name: "Padme",
+        name: 'Padme',
 		healthPoints: 150,
-		attackPower: 45,
-		counterAttackPower: 40,
-        numberOfAttacks: .25
+		attackPower: 20,
+		counterAttackPower: 30,
+        numberOfAttacks: 1
     }
 
     playersArray.push(firstPlayer);
@@ -57,27 +57,21 @@ $(document).ready(function() {
         yourEnemies = false;
         yourDefender = false;
     };
-
     // appending players as your character, enemies and defendent
     function selectOpponents(event) {
         event.preventDefault();
         var $this = $(this);
-        if($this.hasClass('selected')) {
-            return;
-        } else {
-            $this.addClass('selected');
-        }
-
         if (yourCharacter === false) {
             
             $('.player').after($this);
             yourCharacter = playersArray[$this.data('player') - 1];
             yourCharacter.element = $this;
-            $this.addClass('selectedOpponent');
+            $this.addClass('selectedHero');
             $this.removeClass('')
 
             if (yourCharacter) {
                 $('.enemies').after($('.inactive .character'));
+
             }
 
         } else if (yourDefender === false) {
@@ -94,10 +88,10 @@ $(document).ready(function() {
 
     function fight(x, y) {
 
-        // if enemy not selected send an alert
+        // if enemy not selected 
 
         if (yourDefender === false) {
-            alert("Players not selected");
+            $('.status').html('<h1>' + 'Players not selected' + '<h1>');
             return;
         }
 
@@ -117,7 +111,8 @@ $(document).ready(function() {
 
             // check if all enemies are defeated
             if (enemyDefeatedCount === 3) {
-                $('.won').html('<h1>' + 'You Won' + '</h1>')
+            	$('.attack').hide();
+                $('.won').html('<h1>' + 'You Won! Hit ' + '<button>' + '<h2>' + 'Restart' + '</h2>' + '</button>' + ' if you want to play again!' + '</h1>');
             }
 
         } else {
@@ -128,16 +123,15 @@ $(document).ready(function() {
         if (x.healthPoints <= 0) {
             x.element.hide();
             $('.attack').hide();
-            alert('You Lost, please refresh')
+            $('.lost').html('<h1>' + 'You Lost, hit ' + '<button>' + '<h2>' + 'Restart' + '</h2>' + '</button>' + ' to try again!' + '</h1>');
             return;
         }
 
-        $('.playerAttacks').html('You attacked ' + y.name + ' for ' + attackPowerIncrement);
-        $('.defenderAttacks').html(y.name + ' attacked you for ' + y.counterAttackPower);
+        $('.playerAttacks').html('You attacked ' + y.name + ' for ' + attackPowerIncrement + ' damage.');
+        $('.defenderAttacks').html(y.name + ' attacked you for ' + y.counterAttackPower + ' damage.');
     };
 
     // changing function names
-
     $('.character').on('click', selectOpponents);
     $('.attack').on('click', function() {
         fight(yourCharacter, yourDefender);
